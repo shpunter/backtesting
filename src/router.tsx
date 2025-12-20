eee682356140840ba652619b7340698b08f46bea
Oleksandr Brudnovskyi <oleksandr.brudnovskyi@customertimes.com>
2025-12-20 15:02:20 +0100

    add broadcast-channel-middleware

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,12 @@
-import { createRouter } from '@tanstack/react-router'
+import { createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
-
-// Create a new router instance
 export const getRouter = () => {
   const router = createRouter({
     routeTree,
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
-  })
+  });
 
-  return router
-}
+  return router;
+};
